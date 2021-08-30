b5a69a8d7504fd0d5a0603b575002e29cfe33de9
sangpham27 <9353205-sangpham27@users.noreply.gitlab.com>
2021-08-30 16:22:42 +0700

    update Dog.js

--- Dog.js
+++ Dog.js
@@ -1,17 +1,14 @@
-function Dog(name)
-{
-	this.name = name;
-	this.stomach = [];
+function Dog(name) {
+  this.name = name;
+  this.stomach = [];
 }
 
-Dog.prototype.eat = function(cat){
-	this.stomach.push(cat)
-}
+Dog.prototype.eat = function (cat) {
+  this.stomach.push(cat);
+};
 
-Dog.prototype.sayHi = function(){
-
-	return `Dog say hi ${this.name}`;
-}
-
+Dog.prototype.sayHi = function () {
+  console.log(`Dog say hi ${this.name}`);
+};
 
 module.exports = Dog;
---
+++ index.js
@@ -0,0 +1,5 @@
+const Dog = require("./Dog");
+
+let dog1 = new Dog("dog1");
+
+dog1.sayHi();
